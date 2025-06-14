e29ff08a58663d3e1e863e4b79546027cbf93e8d
Suraj-729 <surajkumarsahoo729@gmail.com>
2025-06-14 15:05:00 +0530

    chages in app

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on the port: ${port}`);
+    console.log(`Server is on th running  serverrr on the port: ${port}`);
 });
